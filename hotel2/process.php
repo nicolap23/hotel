83557d521efb32ff64a6e39fc5121648eaa24cca
nicolap23 <136933666+nicolap23@users.noreply.github.com>
2023-11-13 23:45:54 -0300

    Add files via upload

---
+++ hotel2/process.php
@@ -0,0 +1,23 @@
+<?php
+
+$name = $_POST['name'];
+$rut = $_POST['rut'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+
+// Database connection
+$conn = new mysqli('localhost','root','','prueba');
+if($conn->connect_error){
+	echo "$conn->connect_error";
+	die("Connection Failed : ". $conn->connect_error);
+} else {
+	$stmt = $conn->prepare("insert into hotel132(name, rut, email, phone)values( ?, ?, ?, ?)");
+	$stmt->bind_param("sssi", $name, $rut,  $email, $phone);
+	$execval = $stmt->execute();
+	echo $execval;
+	echo "Registration successfully...";
+	$stmt->close();
+	$conn->close();
+}
+?>
+?>
